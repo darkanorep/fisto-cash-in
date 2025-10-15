bc81962f763f996a2c3b930cf063a66e520b6c08
jperona <jeromeperona@yahoo.com>
2025-10-15 15:39:30 +0800

    Implement admin authorization using Gate in AdminMiddleware and define admin policy in AuthServiceProvider

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use App\Models\Role;
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 use Symfony\Component\HttpFoundation\Response;
 
 class AdminMiddleware
@@ -14,11 +15,17 @@ class AdminMiddleware
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next)
     {
-        if (auth()->check() && auth()->user()->roles->contains('name', Role::ADMIN)) {
-                return $next($request);
-            }
-            return response()->json(['message' => 'Forbidden'], 403);
+        // if (auth()->check() && auth()->user()->roles->contains('name', Role::ADMIN)) {
+        //         return $next($request);
+        //     }
+        //     return response()->json(['message' => 'Forbidden'], 403);
+
+        if (auth()->check() && Gate::allows('admin')) {
+            return $next($request);
+        }
+
+        return response()->json(['message' => 'Forbidden'], 403);
     }
 }
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,10 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -21,6 +25,10 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // $this->registerPolicies();
+
+        Gate::define('admin', function (User $user) {
+            return $user->roles->contains('name', Role::ADMIN);
+        });
     }
 }
