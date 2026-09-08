4fade20b5b7d847eead1918909f694b8b7024e19
jperona <jeromeperona@yahoo.com>
2026-09-08 09:18:44 +0800

    refactor (enable authentication middleware for admin routes in api.php)

--- routes/api.php
+++ routes/api.php
@@ -37,14 +37,10 @@ use Illuminate\Support\Facades\Route;
 //Authentication
 Route::post('login', [AuthController::class, 'login']);
 
-Route::group([
-//    'middleware' => 'auth:sanctum'
-], function() {
+Route::group(['middleware' => 'auth:sanctum'], function() {
 
     //Admin Routes
-    Route::group([
-//        'middleware' => 'admin'
-    ], function() {
+    Route::group(['middleware' => 'admin'], function() {
         //Password Reset
         Route::patch('reset-password/{id}', [AuthController::class, 'resetPassword']);
 
